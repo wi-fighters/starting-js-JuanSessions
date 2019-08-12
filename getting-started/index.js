2d52ba0bf46640a08fec3a8b600a8d08d8d4879a
JuanSessions <juanperusin@yahoo.es>
2019-08-12 16:11:32 +0200

    first assignment

---
+++ getting-started/index.js
@@ -0,0 +1,9 @@
+var firstName = "Juan";
+var lastName = "Perusin";
+console.log(firstName, lastName);
+var name = "Johanes"; 
+var city = "Berlin";
+var job = "Musiker";
+var hobby = "Football";
+var favdish = "Meat";
+console.log(name, city, job, hobby, favdish); 
