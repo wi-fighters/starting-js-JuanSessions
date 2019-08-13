d43fdfd53ff4bd88787582e9c086d89c28f9a72d
JuanSessions <juanperusin@yahoo.es>
2019-08-13 14:00:35 +0200

    add exercises tu ghub

--- getting-started/index.js
+++ getting-started/index.js
@@ -1,9 +1,55 @@
-var firstName = "Juan";
-var lastName = "Perusin";
+//1
+let firstName = "Juan";
+let lastName = "Perusin";
 console.log(firstName, lastName);
-var name = "Johanes"; 
-var city = "Berlin";
-var job = "Musiker";
-var hobby = "Football";
-var favdish = "Meat";
-console.log(name, city, job, hobby, favdish); 
+
+//2
+let name = "Johanes"; 
+let city = "Berlin";
+let job = "Musiker";
+let hobby = "Football";
+let favdish = "Meat";
+console.log(name, city, job, hobby, favdish) 
+console.log("This is "+name+" "+"and he lives in "+city)
+
+//3
+let length = 16.78; //Number 
+let lastlastName = "Johnson"; //String
+let car = ""; //String
+let isOpen = true; //Boolean
+console.log(typeof length, typeof lastlastName, typeof car, typeof isOpen)
+
+//4
+let ageMark = "35";
+let ageJohn = 40;
+console.log(ageMark>ageJohn ? "John is older than Mark: true" : "John is older than Mark: false")
+
+//5 
+let a = 10;
+let b = 10;
+let c = 20;
+console.log(a, b, c)
+console.log(a === b, c !== b, a === c)
+
+//6
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun);
+
+//7
+let test;
+console.log(typeof test); 
+
+//8
+let Calculation = (2 * 5) + 4 / (2 - 5);
+console.log("Calculation result:", Calculation); 
+
+let welcome = "Hello";
+let anothername = "Fran";
+console.log(welcome, anothername)
+console.log(`${welcome}, ${anothername}'s friend
+ I want to say "Hi"!`) 
+
+//length property
+console.log(anothername[4]);
+
