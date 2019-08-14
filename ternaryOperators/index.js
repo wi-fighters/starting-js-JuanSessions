1a55f0af0b9f30af7eb89f10e830c643490af819
JuanSessions <juanperusin@yahoo.es>
2019-08-14 11:24:48 +0200

    ternaryOperators

---
+++ ternaryOperators/index.js
@@ -0,0 +1,27 @@
+//1
+const legalAge = 18;
+const personAge = 16;
+const canDrive = (personAge >= legalAge) ? "yes, you can drive" : "no, you cannot drive";
+console.log(canDrive);
+
+//2
+const legalSpeed = 130;
+const personSpeed = 130;
+const speedCheck = (personSpeed <= legalSpeed) ? "You're going too fast - slow down!" : "You're driving below the speed limit";
+console.log(speedCheck);
+
+//3
+const wineAge = 18;
+const person1Age = 19;
+const ageAllowed = (person1Age >= wineAge) ? "Here's some juice" : "Here's some wine";
+console.log(ageAllowed);
+
+//4
+const student = true;
+const isStudent = (student) ? "Ticket costs €5,00" : "Ticket costs €12,00";
+console.log(isStudent);
+
+//5
+const morning = true;
+const timeOfDay = (morning) ? "Good Morning" : "Good Evening";
+console.log(timeOfDay);
