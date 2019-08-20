86bc919e5889f81c004379ecc08c11939d3e7c0f
JuanSessions <juanperusin@yahoo.es>
2019-08-20 09:05:09 +0200

    MathObject

---
+++ MathObject/index.js
@@ -0,0 +1,20 @@
+
+console.log(Math.min(3,4, 54, 65, 7, 9, 1, 0, -1, -1.5));
+console.log(Math.max(1, 24, 45, 5,65, 76, 8));
+
+
+console.log(Math.ceil(3321.32321));
+console.log(Math.ceil(326.76));
+console.log(Math.ceil(76788.7));
+console.log(Math.ceil(-9.78));
+console.log(Math.ceil(43.342));
+ 
+console.log(Math.floor(3321.32321));
+console.log(Math.floor(326.76));
+console.log(Math.floor(76788.7));
+console.log(Math.floor(-9.78));
+console.log(Math.floor(28.329));
+
+console.log(Math.floor(Math.random() * 6 + 1)); 
+
+
