b3de53c9c64cfd9484b51d0ed5c5aa2ab485221f
JuanSessions <juanperusin@yahoo.es>
2019-08-28 10:16:07 +0200

    add writing functions

---
+++ writeFunctions/writeFunctions.js
@@ -0,0 +1,18 @@
+function createBand(clothing, lastFoodEaten) {
+  return `name of our band is ${clothing + lastFoodEaten}`;
+}
+console.log(createBand("shirt", "peach"));
+
+function newBand(fruit, car) {
+  const fruitUpperCase = fruit[0].toUpperCase() + fruit.substring(1);
+  const carUppercase = car[0].toUpperCase() + car.substring(1);
+  return `name of our new rock band is ${fruitUpperCase} ${carUppercase}`;
+}
+console.log(newBand("banana", "peugeot"));
+
+function nameSelector() {
+  const classMates = ["Fede", "Jaime", "Juan", "Fran", "Natalia", "Manuel"];
+  const getName = classMates[Math.floor(Math.random() * 5)];
+  return `and the person selected is ${getName}`;
+}
+console.log(nameSelector());
